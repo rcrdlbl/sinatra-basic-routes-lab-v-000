b79321bbf88e95e9fba6ab1ebe0908d38d230749
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-19 19:07:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Daniel."
+  end
+
+  get '/hometown' do
+    "My hometown is Clearwater."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Along."
+  end
+
 end
